07a39d1b56d76d96ba95502b370baf42890df777
ShivaDatta6 <dattu5989@gmail.com>
2025-07-04 17:07:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,5 +16,7 @@ app.use('/api/todos', todoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ Server is running on port ${PORT}`);
+
+
 });
